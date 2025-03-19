4e4132cad51bee36f44a10055cdcc73f8ed4ec47
syeo-1 <seansqyeo@gmail.com>
2025-03-19 13:10:56 -0700

    add test suite for examples of sec form 4 and finish off extraction function

--- common/extract.py
+++ common/extract.py
@@ -5,6 +5,7 @@ A series of functions to aid in extracting data from form 4 text files
 from bs4 import BeautifulSoup
 import requests
 from datetime import datetime, timedelta
+import pytz
 import re
 from itertools import count
 
@@ -17,8 +18,8 @@ HEADERS = {
     'Host': 'www.sec.gov'
 }
 
-REPORTING_REGEX = re.compile(".+\(\d+\) \(Reporting\)")
-DATETIME_REGEX = re.compile("(\d{4}-\d{2}-\d{2})<br/>(\d{2}:\d{2}:\d{2})")
+REPORTING_REGEX = re.compile(r".+\(\d+\) \(Reporting\)")
+DATETIME_REGEX = re.compile(r"(\d{4}-\d{2}-\d{2})<br/>(\d{2}:\d{2}:\d{2})")
 DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 def extract_data_from_SEC_file_url(url_list, extraction_function):
@@ -42,7 +43,7 @@ def extract_non_derivative_form_4_info(soup):
     '''
     takes a soup object of xml data and extracts the following fields from form 4
 
-    returns the data below in a python dictionary
+    returns the data below in a list of python tuples
     ===
     - amount of shares purchased/sold
     - was this a buy or sell (A/D code)
@@ -52,36 +53,127 @@ def extract_non_derivative_form_4_info(soup):
     - transaction time
     - data retrieval time
     '''
+
+    all_extracted_non_derivative_data = []
+    eastern_timezone = pytz.timezone('US/Eastern')
+
+
     reporting_owner_name = soup.select('rptOwnerName')[0].text
-    num_transaction_shares = soup.select('transactionShares > value')[0].text
-    ownership_nature = soup.select('transactionAcquiredDisposedCode > value')[0].text
     issuer_name = soup.select('issuerName')[0].text
     ticker_symbol = soup.select('issuerTradingSymbol')[0].text
-    transaction_share_price = soup.select('transactionPricePerShare > value')[0].text
-    transaction_time = soup.select('nonDerivativeTransaction > transactionDate > value')[0].text
     acceptance_time = soup.select('acceptance-datetime')[0].text.split()[0]
-    # TODO: should have data retrieval time as well
-    print(
-        reporting_owner_name,
-        num_transaction_shares,
-        ownership_nature,
-        issuer_name,
-        ticker_symbol,
-        transaction_share_price,
-        transaction_time,
-        acceptance_time
-    )
-    return (
-        reporting_owner_name,
-        num_transaction_shares,
-        ownership_nature,
-        issuer_name,
-        ticker_symbol,
-        transaction_share_price,
-        transaction_time,
-        acceptance_time
-    )
+    
+    non_derivative_transactions = soup.select('nonDerivativeTransaction')
 
+    for non_derivative_transaction in non_derivative_transactions:
+
+        try:
+            security_title = non_derivative_transaction.select('securityTitle > value')[0].text
+            if security_title == '':
+                security_title = None
+        except:
+            security_title = None
+        
+        try:
+            transaction_date = non_derivative_transaction.select('transactionDate > value')[0].text
+            if transaction_date == '':
+                transaction_date = None
+        except:
+            transaction_date = None
+
+        try:
+            deemed_execution_date = non_derivative_transaction.select('deemedExecutionDate')[0].text
+            if deemed_execution_date == '':
+                deemed_execution_date = None
+
+        except:
+            deemed_execution_date = None
+        try:
+            transaction_code = non_derivative_transaction.select('transactionCoding > transactionCode')[0].text
+            if transaction_code == '':
+                transaction_code = None
+        except:
+            transaction_code = None
+
+        try:
+            num_transaction_shares = non_derivative_transaction.select('transactionShares > value')[0].text
+            if num_transaction_shares == '':
+                num_transaction_shares = None
+        except:
+            num_transaction_shares = None
+
+        try:
+            acquired_or_dispose = non_derivative_transaction.select('transactionAcquiredDisposedCode > value')[0].text
+            if acquired_or_dispose == '':
+                acquired_or_dispose = None
+        except:    
+            acquired_or_dispose = None
+
+        try:
+            transaction_share_price = non_derivative_transaction.select('transactionPricePerShare > value')[0].text
+            if transaction_share_price == '':
+                transaction_share_price = None
+        except:    
+            transaction_share_price = None
+
+        try:
+            amount_owned_after_transaction = non_derivative_transaction.select('postTransactionAmounts > sharesOwnedFollowingTransaction > value')[0].text
+            if amount_owned_after_transaction == '':
+                amount_owned_after_transaction = None
+        except:    
+            amount_owned_after_transaction = None
+
+        try:
+            ownership_form = non_derivative_transaction.select('ownershipNature > directOrIndirectOwnership > value')[0].text
+            if ownership_form == '':
+                ownership_form = None
+        except:  
+            ownership_form = None
+        # extraction_time = datetime.now(eastern_timezone).strftime(DATETIME_FORMAT)
+
+        print(
+            reporting_owner_name,
+            issuer_name,
+            ticker_symbol,
+            acceptance_time,
+            security_title,
+            transaction_date,
+            deemed_execution_date,
+            transaction_code,
+            num_transaction_shares,
+            acquired_or_dispose,
+            transaction_share_price,
+            amount_owned_after_transaction,
+            ownership_form,
+            # extraction_time
+        )
+        print('===')
+
+        all_extracted_non_derivative_data.append([
+            reporting_owner_name,
+            issuer_name,
+            ticker_symbol,
+            acceptance_time,
+            security_title,
+            transaction_date,
+            deemed_execution_date,
+            transaction_code,
+            num_transaction_shares,
+            acquired_or_dispose,
+            transaction_share_price,
+            amount_owned_after_transaction,
+            ownership_form,
+            # extraction_time
+        ])
+
+
+    return all_extracted_non_derivative_data
+
+    # a = soup.find_all('nonDerivativeTransaction')
+    # print(a)
+
+
+    # # TODO: should have data retrieval time as well
 
 def get_reporting_tr_indices(table_row_soup_list):
     indices_list = []
@@ -198,7 +290,10 @@ def is_non_derivative_non_RSU_transaction(form_4_contents):
 
 def main():
     # with open(r'C:\Users\seans\OneDrive\Desktop\data-zoomcamp\sec-form4-project\tests\workday_standard_disposal.xml') as file:
+    # with open(r'C:\Users\seans\OneDrive\Desktop\data-zoomcamp\sec-form4-project\tests\jetblue_RSU_acquisition.xml') as file:
+    # with open(r'C:\Users\seans\OneDrive\Desktop\data-zoomcamp\sec-form4-project\tests\RSU_with_disposal.xml') as file:
     with open(r'C:\Users\seans\OneDrive\Desktop\data-zoomcamp\sec-form4-project\tests\softbank_derivative_acquisition.xml') as file:
+    # with open(r'C:\Users\seans\OneDrive\Desktop\data-zoomcamp\sec-form4-project\tests\draftking_multi_disposal.xml') as file:
         # for line in file:
         #     print(line)
         xml_soup = xml_to_soup(file)
