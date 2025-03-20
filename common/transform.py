57e3e19c9ebf103d381be1fc7e9da30bde769b57
syeo-1 <seansqyeo@gmail.com>
2025-03-19 20:55:46 -0700

    add function for filtering out non derivative form 4 data and some details in MD file

--- common/transform.py
+++ common/transform.py
@@ -11,3 +11,38 @@ def form_4_data_transform(data):
         float(data[4]),
         str(data[5])
     )
+
+def filter_out_form_4_data(all_form_4_data):
+    '''
+        takes a list of form 4 data points and filters out elements that will not be saved.
+        Returns a new list with the data that passes the filter.
+
+        The goal is to only save standard open market purchases or sales of stock.
+
+        The following types of data will not be saved to the database
+
+        - RSU
+        - indirect purchases
+        - derivatives
+        - gifts
+
+    '''
+
+    filtered_form_4_data = []
+
+    for form_4_data in all_form_4_data:
+        transaction_code = form_4_data[7]
+        ownership_form = form_4_data[12]
+
+        # person's ownership must be direct (ie. buyer/seller is actively attempting to buy/sell stock)
+        # the transaction must be a standard Purchase, Sell, or for whatever reason, they report a purchase/sell
+        # earlier than they have to (V code)
+        # for more info: https://www.sec.gov/edgar/searchedgar/ownershipformcodes.html
+        if ownership_form != 'D' and transaction_code not in ['P', 'S', 'V']:
+            continue
+
+        filtered_form_4_data.append(form_4_data)
+
+    return filtered_form_4_data
+
+
